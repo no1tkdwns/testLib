016db3dd8e7fb5c404bbdc7b92968270b055df0a
sjpark <sjpark@avatye.com>
2023-09-22 17:47:18 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TestLib",
-            dependencies: [],
+            dependencies: ["AdPopcornSSP"],
             linkerSettings: [
                 .linkedFramework("AdPopcornSSP")
             ]
@@ -31,3 +31,4 @@ let package = Package(
             dependencies: ["TestLib"]),
     ]
 )
+
